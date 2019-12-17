467de91ef248f1aff11cf64aacdfa33dd217443c
João Victor Huguenin <46404918+frank400@users.noreply.github.com>
2019-12-17 11:21:36 -0300

    Snake Game(pygame)

---
+++ snake game/main.py
@@ -0,0 +1,57 @@
+import math
+import random
+import pygame
+import tkinter as tk
+from tkinter import  messagebox
+
+class cube (object):
+    rows=0
+    w=0
+    def __int__(self,start,dirnx=1,dirny=0,color=(255,0,0)):
+        pass
+    def move(self,dirnx,dirny):
+        pass
+    def draw(self,surface,eyes=False):
+        pass
+
+class snake(object):
+    def ___int___(self,color,pos):
+        pass
+    def move(self):
+        pass
+    def reset(self):
+        pass
+    def addcube(self):
+        pass
+    def draw(self,surface):
+        pass
+
+def drawgrid(w,rows,surface):
+    pass
+
+def redrawindow(surface):
+    pass
+
+def randomsnack(rows,items):
+    pass
+
+def main():
+    width=500
+    height=500
+    rows=20
+
+    win=pygame.display.set_mode((width,height))
+    s=snake((255,0,0),(10,10))
+    flag=True
+    while flag:
+        pygame.time.delay((50))
+        clock.tick
+    pass
+
+rows=
+w=
+h=
+cube.rows=rows
+cube.w=w
+
+main()
